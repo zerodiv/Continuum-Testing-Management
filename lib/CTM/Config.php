d7cedd3bf85e1f4a4b901b7b64002faf40263bc1
bseydioglu <bseydioglu@localhost.localdomain>
2010-05-06 02:10:41 +0800

    Environment changes

--- lib/CTM/Config.php
+++ lib/CTM/Config.php
@@ -1,7 +1,7 @@
 <?php
 class CTM_Config {
-   public static function BASE_DIR() { return '/Users/jorcutt/Continuum-Testing-Management'; }
-   public static function SUITE_DIR() { return '/Users/jorcutt/tmp/ctm_test_runs'; }
+   public static function BASE_DIR() { return dirname(__FILE__) . '/../..'; }
+   public static function SUITE_DIR() { return sys_get_temp_dir() . '/ctm_test_runs'; }
    public static function DEFAULT_TIMEZONE() { return 'America/Los_Angeles'; }
    public static function TIME_FORMAT() { return 'Y/m/d H:i'; }
 }
--- lib/Light/Database/Factory/Config.php
+++ lib/Light/Database/Factory/Config.php
@@ -1,4 +1,4 @@
 <?php
 class Light_Database_Factory_Config {
-   public static function CONFIG_FILE() { return '/Users/jorcutt/Continuum-Testing-Management/etc/db.ini'; }
+   public static function CONFIG_FILE() { return dirname(__FILE__) . '/../../../../etc/db.ini'; }
 }
