2966047acc114fd64c8eeae02f522452d9899cd2
Burak Seydioglu <bseydioglu@localhost.localdomain>
2010-05-14 16:16:07 -0700

    removed command line stuff

--- bin/test_runner.php
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/php -q
-<?php
-
-require_once( dirname( __FILE__ ) . '/../bootstrap.php' );
-require_once( 'CTM/Test/Runner.php' );
-
-$test_runner_obj = new CTM_Test_Runner();
-$test_runner_obj->run();
--- lib/Light/CommandLine/Script/Config.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Light_CommandLine_Script_Config {
-   public static function DEFAULT_TIMEZONE() { return 'America/Los_Angeles'; }
-   public static function TIME_FORMAT() { return 'Y/m/d H:i'; }
-}
