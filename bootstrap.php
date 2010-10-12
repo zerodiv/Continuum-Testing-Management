857a046573f38e8c0ea0afe112feb3d26f64542d
Jason Orcutt <zerodiv@zerodivide.net>
2010-10-12 10:58:47 -0700

    Created v1 installer

--- bootstrap.php
+++ bootstrap.php
@@ -7,5 +7,37 @@
 // environment does not allow modifying the include path at runtime you will need to 
 // modify your php.ini include_path settings accordingly.
 // --------------------------------------------------------------------------------
-$include_path = get_include_path();
-set_include_path( dirname(__FILE__) . '/lib:' . $include_path );
+$includePath = get_include_path();
+set_include_path(dirname(__FILE__) . '/lib:' . $includePath);
+
+// test to see if we have configuration files.
+$configFiles = array(
+   dirname(__FILE__) . '/etc/config.ini',
+   dirname(__FILE__) . '/etc/db.ini'
+);
+
+$missingConfigFile = false;
+foreach ( $configFiles as $configFile ) {
+   // echo "configFile: $configFile\n";
+   if ( ! file_exists($configFile) ) {
+      $missingConfigFile = true;
+   }
+}
+
+$installerDone = false;
+if ( file_exists( dirname(__FILE__) . '/etc/installer.done' ) ) {
+   $installerDone = true;
+}
+
+// echo "missingConfigFile: $missingConfigFile installerDone: $installerDone\n";
+
+if (
+      $missingConfigFile == true || 
+      $installerDone == false || 
+      (isset($_GET['installerTesting']) && $_GET['installerTesting'] == 1)
+   ) {
+   require_once( 'CTM/Installer.php' );
+   $ctmInstallerObj = new CTM_Installer();
+   $ctmInstallerObj->displayPage();
+   exit();
+}
---
+++ lib/CTM/Installer.php
@@ -0,0 +1,85 @@
+<?php
+
+require_once('CTM/Site.php');
+require_once('Light/Database/Connection/Factory.php');
+
+class CTM_Installer extends CTM_Site
+{
+   private $_tables;
+
+   public function setupPage()
+   {
+      $this->setPageTitle('Installer');
+      return true;
+   }
+
+   public function handleRequest() 
+   {
+      // Get a write connection to the database.
+      try {
+         $tableDir = Light_Config::get('CTM_Config', 'base_dir') . '/sql/table';
+         $dataDir = Light_Config::get('CTM_Config', 'base_dir') . '/sql/data';
+
+         $rawTables = scandir($tableDir);
+         $dbTables = array();
+
+         $dbh = Light_Database_Connection_Factory::getDBH('test');
+
+         foreach ( $rawTables as $rawTable ) {
+            $tableFile = $tableDir . '/' . $rawTable;
+            $dataFile = $dataDir . '/' . $rawTable;
+
+            if ( is_file($tableFile) ) {
+               $tableName = str_replace( '.sql', '', $rawTable );
+
+               $sth = $dbh->prepare('SHOW TABLES LIKE ?'); 
+               $sth->bindParam(1, $tableName);
+               $sth->execute(); 
+               
+               $rowCount = $sth->rowCount(); 
+               
+               if ( $rowCount > 0 ) {
+                  $this->_tables[$tableName] = 'existed';
+               } else {
+                  $dbh->exec(file($tableFile));
+                  if ( is_file($dataFile) ) {
+                     $dbh->exec(file($dataFile));
+                  }
+                  $this->_tables[$tableName] = 'created';
+               } 
+            }
+
+         }
+
+      } catch ( Exception $e ) {
+         return false;
+      }
+      return true;
+   }
+
+   public function displayBody()
+   {
+      $this->printHtml('<div class="aiTableContainer aiFullWidth">');
+      $this->printHtml('<table class="ctmTable aiFullWidth">');
+
+      $this->printhtml('<tr>');
+      $this->printHtml('<th colspan="2">CTM Installer</th>');
+      $this->printHtml('</tr>');
+
+      $this->printHtml('<tr>');
+      $this->printHtml('<th>Table Name:</th>');
+      $this->printHtml('<th>Creation Status:</th>');
+      $this->printHtml('</tr>');
+
+      foreach ( $this->_tables as $tableName => $tableStatus ) {
+         $this->printHtml('<tr class="' . $this->oddEvenClass() . '">');
+         $this->printHtml('<td>' . $tableName . '</td>');
+         $this->printHtml('<td><center>' . $tableStatus . '</center></td>');
+         $this->printHtml('</tr>');
+      }
+      $this->printHtml('</table>');
+      $this->printHtml('</div>');
+      return true;
+   }
+
+}
